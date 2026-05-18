0988e39a443be670e49217e82ba46c62a92a38ba
Nightsky_Lty <1048418581@qq.com>
2026-05-18 16:55:29 +0800

    完成RMSNorm模块

--- assignment1-basics-main/cs336_basics/model.py
+++ assignment1-basics-main/cs336_basics/model.py
@@ -2,6 +2,7 @@ import torch
 from torch import Tensor
 from jaxtyping import Float, Int
 from einops import einsum
+from einops import rearrange
 import math
 class Linear(torch.nn.Module):
     def __init__(
@@ -30,7 +31,7 @@ class Embedding(torch.nn.Module):
         embedding_dim: int,
         device: torch.device=None,
         dtype: torch.dtype=None
-        ):
+    ):
         super().__init__()
         w: Float[Tensor, "vocab_size d_model"] = torch.empty(num_embeddings, embedding_dim, device=device, dtype=dtype)
         std = 1.0
@@ -39,5 +40,24 @@ class Embedding(torch.nn.Module):
     
     def forward(self, x: Int[Tensor, "..."]) -> Float[Tensor, "... d_model"]:
         out = self.weight[x]
+        return out
+
+class RMSNorm(torch.nn.Module):
+    def __init__(
+        self,
+        d_model: int,
+        eps: float,
+        device: torch.device=None,
+        dtype: torch.dtype=None    
+    ):
+        super().__init__()
+        self.d_model = d_model
+        self.eps = eps
+        w: Float[Tensor, "d_model"] = torch.ones(d_model,device=device, dtype=dtype)
+        self.weight = torch.nn.Parameter(w)
+
+    def forward(self, x: Float[Tensor, "... d_model"]) -> Float[Tensor, "... d_model"]:
+        RMS = torch.sqrt((x * x).mean(-1, keepdim=True) + self.eps)
+        out = x / RMS * self.weight
         return out
 
--- assignment1-basics-main/tests/adapters.py
+++ assignment1-basics-main/tests/adapters.py
@@ -12,7 +12,7 @@ from torch import Tensor
 from cs336_basics.utils import silu
 from cs336_basics.tokenizer import train_bpe 
 from cs336_basics.tokenizer import Tokenizer
-from cs336_basics.model import Linear, Embedding
+from cs336_basics.model import Linear, Embedding, RMSNorm
 
 def run_linear(
     d_in: int,
@@ -386,6 +386,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
+    rmsnorm = RMSNorm(d_model, eps)
+    rmsnorm.weight.data = weights
+    return rmsnorm(in_features)
     raise NotImplementedError
 
 
