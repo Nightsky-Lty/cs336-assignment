375602e225239e24ed9b96d93b7da246b0584e54
Nightsky_Lty <1048418581@qq.com>
2026-05-18 16:02:48 +0800

    完成Linear类

--- assignment1-basics-main/cs336_basics/model.py
+++ assignment1-basics-main/cs336_basics/model.py
@@ -1,9 +1,24 @@
-import torch.nn as nn
 import torch
-
-class Linear(nn.Module):
-    def __init__(self, in_features: int, out_features: int, device: torch.device = None, type: torch.dtype=None):
+from torch import Tensor
+from jaxtyping import Float
+from einops import einsum
+import math
+class Linear(torch.nn.Module):
+    def __init__(
+        self, 
+        in_features: int, 
+        out_features: int, 
+        device: torch.device = None, 
+        dtype: torch.dtype=None
+        ):
+        super().__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.device = device
-        self.type = self.type
+        w = torch.empty(out_features, in_features, device=device, dtype=dtype)
+        sigma = math.sqrt(2.0 / (1.0 * in_features + out_features))
+        torch.nn.init.trunc_normal_(w, mean=0.0, std=sigma, a=-3.0 * sigma, b=3.0 * sigma)
+        self.weights: Float[Tensor, "d_out d_in"] = torch.nn.Parameter(w)
+    
+    def forward(self, x: Float[Tensor, "... d_in"]) -> Float[Tensor, "... d_out"]:
+        out = einsum(x, self.weights, "... d_in, d_out d_in -> ... d_out")
+        return out
--- assignment1-basics-main/tests/adapters.py
+++ assignment1-basics-main/tests/adapters.py
@@ -12,6 +12,7 @@ from torch import Tensor
 from cs336_basics.utils import silu
 from cs336_basics.tokenizer import train_bpe 
 from cs336_basics.tokenizer import Tokenizer
+from cs336_basics.model import Linear
 
 def run_linear(
     d_in: int,
@@ -31,7 +32,9 @@ def run_linear(
     Returns:
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
-
+    linear = Linear(d_in, d_out)
+    linear.weights.data = weights
+    return linear.forward(in_features)
     raise NotImplementedError
 
 
