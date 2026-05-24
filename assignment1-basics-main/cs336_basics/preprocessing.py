60e0bb95495fafdbfc427d5a2ad9bf372c237c2f
nightsky <1048418581@qq.com>
2026-05-24 23:33:30 +0800

    preprocessing脚本完成

---
+++ assignment1-basics-main/cs336_basics/preprocessing.py
@@ -0,0 +1,65 @@
+from cs336_basics import tokenizer
+import pickle, argparse
+import numpy as np
+
+def train_tokenizer(
+    vocab_path: str,
+    merges_path: str,
+    vocab_size: int,
+    special_tokens: list[str]
+):
+    train_data_path = "../data/TinyStoriesV2-GPT4-train.txt"
+    vocab, merges = tokenizer.train_bpe(train_data_path, vocab_size, special_tokens)
+
+    with open(vocab_path, "wb") as v:
+        pickle.dump(vocab, v)
+    with open(merges_path, "wb") as m:
+        pickle.dump(merges, m)
+
+def encode_txt(
+    txt_path: str,
+    vocab_path: str,
+    merges_path: str,
+    special_tokens: list[str],
+    out_path: str
+):
+    tk = tokenizer.Tokenizer.from_files(vocab_path, merges_path, special_tokens)
+    with open(txt_path, "r", encoding="utf-8") as f:
+        text = f.read()
+    tokens = tk.encode(text)
+    tokens_arr = np.array(tokens, dtype=np.uint16)
+    tokens_arr.tofile(out_path)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", type=str, required=True)
+    parser.add_argument("--txt_path", type=str, required=True)
+    parser.add_argument("--vocab_size", type=int, default=10000)
+    parser.add_argument("--vocab_path", type=str, default="../data/tinystories_vocab.pkl")
+    parser.add_argument("--merges_path", type=str, default="../data/tinystories_merges.pkl")
+    parser.add_argument("--out_path", type=str, default="../data/encode_result.bin")
+
+    args = parser.parse_args()
+
+    special_tokens = ["<|endoftext|>"]
+
+    if args.mode == "train_tokenizer":
+        train_tokenizer(
+            special_tokens=special_tokens,
+            vocab_size=args.vocab_size,
+            vocab_path=args.vocab_path,
+            merges_path=args.merges_path
+        )
+    elif args.mode == "encode_txt":
+        encode_txt(
+            txt_path=args.txt_path,
+            vocab_path=args.vocab_path,
+            merges_path=args.merges_path,
+            special_tokens=special_tokens,
+            out_path=args.out_path
+        )
+    else:
+        raise ValueError("Invalid mode")
+
+if __name__ == "__main__":
+    main()
